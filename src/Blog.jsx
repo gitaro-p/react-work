346fde0a079bc90fffa7484e122fc5d6c6e9edaa
清川　裕太郎 <yuu7353@gmail.com>
2020-09-26 23:59:04 +0900

    Hooksでstateを使う

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,54 +1,43 @@
 import React from 'react';
 import Article from './Article';
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    };
-  }
+const Blog = () => {
+  // componentDidMount() {
+  //   //ボタンがクリックされたらいいねをカウントアップする
+  //   document.getElementById('counter').addEventListener('click', this.countUp);
+  // }
 
-  componentDidMount() {
-    //ボタンがクリックされたらいいねをカウントアップする
-    document.getElementById('counter').addEventListener('click', this.countUp);
-  }
+  // componentDidUpdate() {
+  //   if (this.state.count >= 10) {
+  //     this.setState({ count: 0 });
+  //   }
+  // }
 
-  componentDidUpdate() {
-    if (this.state.count >= 10) {
-      this.setState({ count: 0 });
-    }
-  }
+  // componentWillUnmount() {
+  //   document
+  //     .getElementById('counter')
+  //     .removeEventListener('click', this.countUp);
+  // }
 
-  componentWillUnmount() {
-    document
-      .getElementById('counter')
-      .removeEventListener('click', this.countUp);
-  }
+  // //　公開状態を反転させる関数
+  // togglePublished = () => {
+  //   this.setState({
+  //     isPublished: !this.state.isPublished
+  //   });
+  // };
 
-  //　公開状態を反転させる関数
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished
-    });
-  };
+  // countUp = () => {
+  //   this.setState({ count: this.state.count + 1 });
+  // };
 
-  countUp = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+  return (
+    <>
+      <Article
+        title={'Reactの学習'}
+        //count={this.state.count}
+      />
+    </>
+  );
+};
 
-  render() {
-    return (
-      <>
-        <Article
-          title={'Reactの学習'}
-          isPublished={this.state.isPublished}
-          toggle={() => this.togglePublished()}
-          count={this.state.count}
-        />
-      </>
-    );
-  }
-}
 export default Blog;
